5ec560c4e0f80cd0f801dcfa758aa309c9741fc2
Nico Seddio <40968787+Nikorokia@users.noreply.github.com>
2020-01-01 18:14:40 -0500

    Update script_export.lua
    added support for multiple layout modifiers within one M-key keymap.  copied number and symbol layouts to the M2 keymap, effectively making the entire one-handed keyboard available under the M2 key. improved error-checking for unmapped/unsupported key presses.

--- Default_Profile/script_export.lua
+++ Default_Profile/script_export.lua
@@ -5,22 +5,21 @@ function OnEvent(event, arg)
 		MState = GetMKeyState("lhc")
 		GKey = arg
 
-		-- only continue if a valid M-Key state
+		-- only continue if a valid scripted M-Key state
 		if (Keymaps[MState].HandledByScript) then
 
 			-- if key is a layout modifier (mirroring, etc), set the modifier toggle
 			if (Keymaps[MState].LayoutModifiers[GKey] ~= nil) then
-				LayoutModifier = true
+				LayoutModifier = Keymaps[MState].LayoutModifiers[GKey].Layout
 			-- if key is a key modifier (shift, control, etc), press the key
-			elseif (Keymaps[MState].KeyModifiers[GKey] == true) then
+			elseif (Keymaps[MState].KeyModifiers[GKey] ~= nil) then
 				PressKey(Keymaps[MState][1][GKey])
-			-- if a layout modifier is being pressed, press and release the key from the appropriate layout
-			elseif (LayoutModifier) then
-				PressAndReleaseKey(Keymaps[MState][2][GKey])
-				LayoutModifierUsed = true
-			-- if no other weirdness, press and release the key from the base layout
-			else
-				PressAndReleaseKey(Keymaps[MState][1][GKey])
+			-- otherwise, press and release the key from the appropriate layout
+			elseif (Keymaps[MState][LayoutModifier][GKey] ~= nil) then
+				PressAndReleaseKey(Keymaps[MState][LayoutModifier][GKey])
+				if (LayoutModifier ~= 1) then
+					LayoutModifierUsed = true
+				end
 			end
 		end
 	end
@@ -30,20 +29,20 @@ function OnEvent(event, arg)
 		MState = GetMKeyState("lhc")
 		GKey = arg
 
-		-- only continue if a valid M-Key state
+		-- only continue if a valid scripted M-Key state
 		if (Keymaps[MState].HandledByScript) then
 
-			-- if key is a layout modifier (mirroring, etc), remove the modifier toggle
+			-- if key is a layout modifier (mirroring, etc), set the modifier to default
 			if (Keymaps[MState].LayoutModifiers[GKey] ~= nil) then
-				LayoutModifier = false
+				LayoutModifier = 1
 				-- if no other keys were pressed while the layout was modified,
-				--    press and release mod key's unmodded function
-				if (not LayoutModifierUsed) then
-					PressAndReleaseKey(Keymaps[MState][1][GKey])
+				--    press and release mod key's unmodded function if applicable
+				if (not LayoutModifierUsed and Keymaps[MState].LayoutModifiers[GKey].SecondaryFunction) then
+					PressAndReleaseKey(Keymaps[MState][LayoutModifier][GKey])
 				end
 				LayoutModifierUsed = false
 			-- if key is a key modifier (shift, control, etc), release the key
-			elseif (Keymaps[MState].KeyModifiers[GKey] == true) then
+			elseif (Keymaps[MState].KeyModifiers[GKey] ~= nil) then
 				ReleaseKey(Keymaps[MState][1][GKey])
 			end
 		end
@@ -54,7 +53,7 @@ end
 
 MState = 2
 GKey = 0
-LayoutModifier = false
+LayoutModifier = 1
 LayoutModifierUsed = false
 
 Keymaps = {
@@ -65,16 +64,21 @@ Keymaps = {
 		KeyModifiers = {
 		},
 		[1] = {
-		}
+		},
 	},
 	[2] = { -- M2 Keymaps
 		HandledByScript = true,
 		LayoutModifiers = {
 			[22] = {
-				Active = false,
-				Used = false,
-				Layout = 2
-			}
+				Layout = 2,
+				SecondaryFunction = true,
+			},
+			[23] = {
+				Layout = 3,
+			},
+			[24] = {
+				Layout = 4,
+			},
 		},
 		KeyModifiers = {
 			[ 8] = true;
@@ -82,17 +86,45 @@ Keymaps = {
 			[21] = true;
 		},
 		[1] = {
-			[ 1] = "escape";	[ 2] = "q";		[ 3] = "w";	[ 4] = "e";	[ 5] = "r";	[ 6] = "t";	[ 7] = "backspace";
-			[ 8] = "lshift";	[ 9] = "a";		[10] = "s";	[11] = "d";	[12] = "f";	[13] = "g";	[14] = "enter";
-			[15] = "z";		[16] = "x";		[17] = "c";	[18] = "v";	[19] = "b";
-			[20] = "lctrl";	[21] = "lalt"; 	[22] = "spacebar";
+			[ 1] = "escape",	[ 2] = "q",		[ 3] = "w",	[ 4] = "e",	[ 5] = "r",	[ 6] = "t",	[ 7] = "backspace",
+			[ 8] = "lshift",	[ 9] = "a",		[10] = "s",	[11] = "d",	[12] = "f",	[13] = "g",	[14] = "enter",
+			[15] = "z",		[16] = "x",		[17] = "c",	[18] = "v",	[19] = "b",
+			[20] = "lctrl",	[21] = "lalt", 	[22] = "spacebar",
+			[23] = nil,						[26] = "up",
+							[29] = "left",	[25] = nil,	[27] = "right",
+											[28] = "down",
+			[24] = nil,
 		},
 		[2] = {
-			[ 1] = "tab";		[ 2] = "p";			[ 3]  = "o";		[ 4] = "i";	[ 5] = "u";	[ 6] = "y";	[ 7] = "delete";
-			[ 8] = "";	[ 9] = "semicolon";	[10] = "l";		[11] = "k";	[12] = "j";	[13] = "h";	[14] = "enter";
-			[15] = "slash";	[16] = "period";		[17] = "comma";	[18] = "m";	[19] = "n";
-			[20] = "";	[21] = "";		[22] = "spacebar";
-		}
+			[ 1] = "tab",		[ 2] = "p",			[ 3]  = "o",		[ 4] = "i",	[ 5] = "u",	[ 6] = "y",	[ 7] = "delete",
+			[ 8] = nil,		[ 9] = "semicolon",	[10] = "l",		[11] = "k",	[12] = "j",	[13] = "h",	[14] = "enter",
+			[15] = "slash",	[16] = "period",		[17] = "comma",	[18] = "m",	[19] = "n",
+			[20] = nil,		[21] = nil,			[22] = "spacebar",
+			[23] = nil,							[26] = nil,
+							[29] = nil,			[25] = nil,		[27] = nil,
+												[28] = nil,
+			[24] = nil,
+		},
+		[3] = {
+			[1]  = "tilde",	[2]  = "1",			[3]  = "2",		[4]  = "3",		[5]  = "4",		[6]  = "5",		[7]  = "backspace",
+			[8]  = nil,		[9]  = "non_us_slash",	[10] = "rbracket",	[11] = "lbracket",	[12] = "equal",	[13] = "minus",	[14] = "enter",
+			[15] = "quote",	[16] = nil,			[17] = nil,		[18] = nil,		[19] = nil,
+			[20] = "lctrl",	[21] = "lalt",		[22] = "spacebar",
+			[23] = nil,							[26] = nil,
+							[29] = nil,			[25] = nil,		[27] = nil,
+												[28] = nil,
+			[24] = nil,
+		},
+		[4] = {
+			[1]  = "escape",	[2]  = "0",	[3]  = "9",	[4]  = "8",	[5]  = "7",	[6]  = "6",	[7]  = "delete",
+			[8]  = nil,		[9]  = nil,	[10] = nil,	[11] = nil,	[12] = nil,	[13] = nil,	[14] = "enter",
+			[15] = nil,		[16] = nil,	[17] = nil,	[18] = nil,	[19] = nil,
+			[20] = nil,		[21] = nil,	[22] = nil,
+			[23] = nil,					[26] = nil,
+							[29] = nil,	[25] = nil,	[27] = nil,
+										[28] = nil,
+			[24] = nil,
+		},
 	},
 	[3] = { -- M3 Keymaps
 		HandledByScript = true,
@@ -101,24 +133,32 @@ Keymaps = {
 				Active = false,
 				Used = false,
 				Layout = 2
-			}
+			},
 		},
 		KeyModifiers = {
-			[ 8] = true;
-			[20] = true;
-			[21] = true;
+			[ 8] = true,
+			[20] = true,
+			[21] = true
 		},
 		[1] = {
-			[1]  = "tilde";	[2]  = "1";	[3]  = "2";	[4]  = "3";	[5]  = "4";	[6]  = "5";	[7]  = "backspace";
-			[8]  = "lshift";	[9]  = "non_us_slash";	[10] = "rbracket";	[11] = "lbracket";	[12] = "equal";	[13] = "minus";	[14] = "enter";
-			[15] = "quote";	[16] = "";	[17] = "";	[18] = "";	[19] = "";
-			[20] = "lctrl";	[21] = "lalt";	[22] = "spacebar";
+			[1]  = "tilde",	[2]  = "1",			[3]  = "2",		[4]  = "3",		[5]  = "4",		[6]  = "5",		[7]  = "backspace",
+			[8]  = "lshift",	[9]  = "non_us_slash",	[10] = "rbracket",	[11] = "lbracket",	[12] = "equal",	[13] = "minus",	[14] = "enter",
+			[15] = "quote",	[16] = nil,			[17] = nil,		[18] = nil,		[19] = nil,
+			[20] = "lctrl",	[21] = "lalt",		[22] = "spacebar",
+			[23] = nil,							[26] = nil,
+							[29] = nil,			[25] = nil,		[27] = nil,
+												[28] = nil,
+			[24] = nil,
 		},
 		[2] = {
-			[1]  = "escape";	[2]  = "0";	[3]  = "9";	[4]  = "8";	[5]  = "7";	[6]  = "6";	[7]  = "delete";
-			[8]  = "";	[9]  = "";	[10] = "";	[11] = "";	[12] = "";	[13] = "";	[14] = "enter";
-			[15] = "";	[16] = "";	[17] = "";	[18] = "";	[19] = "";
-			[20] = "";	[21] = "";	[22] = "";
+			[1]  = "escape",	[2]  = "0",	[3]  = "9",	[4]  = "8",	[5]  = "7",	[6]  = "6",	[7]  = "delete",
+			[8]  = nil,		[9]  = nil,	[10] = nil,	[11] = nil,	[12] = nil,	[13] = nil,	[14] = "enter",
+			[15] = nil,		[16] = nil,	[17] = nil,	[18] = nil,	[19] = nil,
+			[20] = nil,		[21] = nil,	[22] = nil,
+			[23] = nil,					[26] = nil,
+							[29] = nil,	[25] = nil,	[27] = nil,
+										[28] = nil,
+			[24] = nil,
 		}
 	}
 }
